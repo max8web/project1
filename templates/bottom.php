f33da043dab176ddca6d5141b10950b60afc1a75
helen <kotik04@gmail.com>
2016-02-13 10:09:49 +0300

    first commit

---
+++ templates/bottom.php
@@ -0,0 +1,19 @@
+</div>
+			<div class="col-md-3">
+				 <table class="my_table1" width="100%" height="180">
+				<tr><td><a href="http://6.pogoda.by/26850" target="_blank">Погода</a></td></tr>
+				<tr><td>
+				<table class="my_table1" width="100%" height="100%"><tr><td>
+				<script type="text/javascript" charset="windows-1251" src="http://pogoda.by/meteoinformer/js/26850_1.js"></script>
+				</td></tr>
+				</table>
+				</td></tr>
+				</table>
+			</div>
+		</div>
+		<br style="clear:both;">
+		<div class="footer">
+		Copyright &copy; max-web - 2016
+		</div>
+	</body>
+</html>
---
+++ templates/top.php
@@ -0,0 +1,33 @@
+<!Doctype>
+<html>
+	<head>
+		<title>Мой проект</title>
+		<meta charset="utf-8">
+		<meta name="description" content="Описание">
+		<meta name="keywords" content="Ключи">
+		<meta name="author" content="Контактная инфо">
+		<link type="text/css" rel="stylesheet" href="media/css/bootstrap.min.css" />
+		<link type="text/css" rel="stylesheet" href="media/css/style.css" />
+	</head>
+	<body>
+		<div class="header">
+			<img src="media/img/logo.png" class="logo" />
+			<h1 class="logotext">Разработка сайтов на PHP</h1>
+		</div>
+		<nav class="topmenu">
+			<a href="#">Главная</a>
+			<a href="#">О компании</a>
+			<a href="#">Продукция</a>
+			<a href="#">Вакансии</a>
+			<a href="#">Контакты</a>
+		</nav>
+		<div class="main">
+			<div class="col-md-3">				
+				<a href="#" class="btn btn-primary btn-block">Разработка сайтов</a>
+				<a href="#" class="btn btn-primary btn-block">Продвижение</a>
+				<a href="#" class="btn btn-primary btn-block">Доработка</a>
+				<a href="#" class="btn btn-primary btn-block">Тестирование</a>
+				<a href="#" class="btn btn-primary btn-block">Поддержка</a>
+				
+			</div>
+			<div class="col-md-6">	
