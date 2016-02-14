b578aa843f0f798f681d157c20b9fdf8516aff85
max8web <max-web@ya.ru>
2016-02-14 17:56:27 +0300

    14.02.06 17-55

--- templates/bottom.php
+++ templates/bottom.php
@@ -1,7 +1,7 @@
 </div>
 			<div class="col-md-3">
-				 <table class="my_table1" width="100%" height="180">
-				<tr><td><a href="http://6.pogoda.by/26850" target="_blank">Погода</a></td></tr>
+				<table class="my_table0" width="100%" height="180">
+				<tr><td class="my_table1_td1"><a href="http://6.pogoda.by/26850" target="_blank">Погода</a></td></tr>
 				<tr><td>
 				<table class="my_table1" width="100%" height="100%"><tr><td>
 				<script type="text/javascript" charset="windows-1251" src="http://pogoda.by/meteoinformer/js/26850_1.js"></script>
