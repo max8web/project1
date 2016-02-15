46ae6799e647fb183be9b83d47cd9d995d7514de
max8web <max-web@ya.ru>
2016-02-15 10:43:13 +0300

    15.02.16 10-45

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+	$db_location='localhost:3307';
+	$db_name='max-web';
+	$db_user='root';
+	$db_password='';
+	$db_con=mysql_connect ($db_location, $db_user, $db_password);
+	if (!$db_con) { 
+		exit ('no connect server');
+	}
+	$db_sel=mysql_select_db ($db_name, $db_con);
+	if (!$db_sel) {
+		exit ('no connect db');
+	}
+	@mysql_query ("SET NAMES 'utf8'");
+?>
